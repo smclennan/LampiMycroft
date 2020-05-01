8264f78ebe3ade7e0a44018523a68953f289adda
Sean McLeannan <007smclennn@gmail.com>
2020-05-01 15:30:49 +0100

    Initial Commit

---
+++ __init__.py
@@ -0,0 +1,22 @@
+from adapt.intent import IntentBuilder
+from mycroft import MycroftSkill, intent_handler
+
+class LampiSkill(MycroftSkill):
+    def __init__(self):
+        super().__init__()
+        self.learning = True
+
+    def initialize(self):
+        my_setting = self.settings.get('my_setting')
+
+
+    @intent_handler(IntentBuilder('SetHueIntent').require('SetHue')
+    def handle_set_hue_intent(self, message):
+        self.speak_dialog("test.one")
+
+    def stop(self):
+        pass
+
+
+def create_skill():
+    return LampiSkill()
